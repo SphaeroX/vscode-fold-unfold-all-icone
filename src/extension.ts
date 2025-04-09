3831fda5818430acb8d4c96dfd63b7a1dc308775
SphaeroX <sphaerox@gmail.com>
2025-04-09 10:29:27 +0200

    refactor: update fold and unfold commands in status bar for consistency

--- src/extension.ts
+++ src/extension.ts
@@ -6,32 +6,18 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate({ subscriptions }: vscode.ExtensionContext) {
 	const foldStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	foldStatusBarItem.command = "editor.foldAll";
-	foldStatusBarItem.tooltip = "fold all";
+	foldStatusBarItem.command = "extension.foldSelected";
+	foldStatusBarItem.tooltip = "Fold all or selected";
 	foldStatusBarItem.text = `{$(chevron-right)$(chevron-left)}`;
 	subscriptions.push(foldStatusBarItem);
 	foldStatusBarItem.show();
 
 	const unfoldStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	unfoldStatusBarItem.command = "editor.unfoldAll";
-	unfoldStatusBarItem.tooltip = "unfold all";
+	unfoldStatusBarItem.command = "extension.unfoldSelected";
+	unfoldStatusBarItem.tooltip = "Unfold all or selected";
 	unfoldStatusBarItem.text = `{$(chevron-left)$(chevron-right)}`;
 	subscriptions.push(unfoldStatusBarItem);
 	unfoldStatusBarItem.show();
-
-	const foldSelectedStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	foldSelectedStatusBarItem.command = "extension.foldSelected";
-	foldSelectedStatusBarItem.tooltip = "Fold Selected";
-	foldSelectedStatusBarItem.text = "$(chevron-right)$(selection)$(chevron-left)";
-	subscriptions.push(foldSelectedStatusBarItem);
-	foldSelectedStatusBarItem.show();
-
-	const unfoldSelectedStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	unfoldSelectedStatusBarItem.command = "extension.unfoldSelected";
-	unfoldSelectedStatusBarItem.tooltip = "Unfold selected lines";
-	unfoldSelectedStatusBarItem.text = `$(chevron-left)$(selection)$(chevron-right)`;
-	subscriptions.push(unfoldSelectedStatusBarItem);
-	unfoldSelectedStatusBarItem.show();
 
 	// Command 'extension.unfoldSelected' registrieren
 	const unfoldSelectedDisposable = vscode.commands.registerCommand('extension.unfoldSelected', () => {
